f1b182c2c957c21944bd4ed0f8d9605140bb8892
Anas Rabea <anas.rabea100@gmail.com>
2022-12-09 20:43:09 +0200

    booking page

--- js/script.js
+++ js/script.js
@@ -120,3 +120,24 @@ setInterval(function () {
     }
 }, 5000);
 // ===================================================================================================
+
+var indexValue = 1;
+showImg(indexValue);
+
+function btm_slide(e){showImg(indexValue = e);}
+function side_slide(e){showImg(indexValue += e);}
+function showImg(e){
+    var i;
+    const img = document.querySelectorAll('.carousel-item img');
+    const sliders = document.querySelectorAll('.row-dots span');
+    if(e > img.length){indexValue = 1}
+    if(e <1){indexValue = img.length}
+    for(i = 0; i < img.length; i++) {
+        img[i].style.display = "none";
+    }
+    for(i = 0; i < sliders.length; i++) {
+        sliders[i].style.background = "rgba(255,255,255,.5)";
+    }
+    img[indexValue-1].style.display = "block";
+    sliders[indexValue-1].style.background = "#10b759";
+}
